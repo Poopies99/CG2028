c85fb57b1dbd67ca6cf8ba4a374a4dbe4e0ba3a4
Poopies99 <>
2022-03-23 12:36:07 +0800

    second commit

---
+++ CG2028AssignS1AY2122/Debug/src/cr_startup_lpc17.d
@@ -0,0 +1,4 @@
+src/cr_startup_lpc17.d: ../src/cr_startup_lpc17.c \
+ D:\MyWork1\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h
+
+D:\MyWork1\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
---
+++ Lib_CMSISv1p30_LPC17xx/Debug/src/system_LPC17xx.d
@@ -0,0 +1,10 @@
+src/system_LPC17xx.d: ../src/system_LPC17xx.c \
+ D:\MyWork1\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+ D:\MyWork1\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h \
+ D:\MyWork1\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h
+
+D:\MyWork1\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+
+D:\MyWork1\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h:
+
+D:\MyWork1\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
