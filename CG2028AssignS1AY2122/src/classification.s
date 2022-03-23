26bf2ef13fe05cbdcd21c4aa88d0234a7c73b865
Topher <69495787+Poopies99@users.noreply.github.com>
2022-03-23 17:11:31 +0800

    Improve Readability

--- CG2028AssignS1AY2122/src/classification.s
+++ CG2028AssignS1AY2122/src/classification.s
@@ -28,42 +28,42 @@ classification:
 @ write asm function body here
 		MOV R8, #0 						@E0A08000
 		MOV R9, #0 						@E0A09000
-		L1:	LDR R4, [R0] 				@0x05904000
+		L1:	LDR R4, [R0] 					@0x05904000
 			ADD R0, #4 					@0x02800004
-			LDR R5, [R1] 				@0x05915000
+			LDR R5, [R1] 					@0x05915000
 			ADD R1, #4 					@0x02811004
 			SUB R4, R5 					@0x00444005
 			MUL R4, R4 					@0x00004414
-			LDR R6, [R0] 				@0x05906000
+			LDR R6, [R0] 					@0x05906000
 			SUB R0, #4 					@0x02400004
-			LDR R5, [R1]				@0x05915000
+			LDR R5, [R1]					@0x05915000
 			ADD R1, #4 					@0x02811004
 			SUB R6, R5 					@0x00466005
-			MLA R6, R6, R6, R4 			@0x00246616
+			MLA R6, R6, R6, R4 				@0x00246616
 
-			LDR R4, [R0] 				@0x05904000
+			LDR R4, [R0] 					@0x05904000
 			ADD R0, #4 					@0x02800004
-			LDR R5, [R1] 				@0x05915000
+			LDR R5, [R1] 					@0x05915000
 			ADD R1, #4 					@0x02811004
 			SUB R4, R5 					@0x00444005
 			MUL R4, R4 					@0x00004414
-			LDR R5, [R0] 				@0x05905000
+			LDR R5, [R0] 					@0x05905000
 			ADD R0, #4 					@0x02800004
-			LDR R7, [R1] 				@0x05917000
+			LDR R7, [R1] 					@0x05917000
 			SUB R5, R7 					@0x00455007
-			MLA R5, R5, R5, R4 			@0x00245515
+			MLA R5, R5, R5, R4 				@0x00245515
 
 			CMP R6, R5 					@0x01560005
-			BMI CLASS_0 				@0x48800008
+			BMI CLASS_0 					@0x48800008
 			ADD R9, #1 					@0x02899001
-			B SKIPCLASS_0 				@0xE8800004
-		CLASS_0: ADD R8, #1 ADD R9, #1 	@0x02888001
-		SKIPCLASS_0: CMP R3, #0 		@0x03530000
+			B SKIPCLASS_0 					@0xE8800004
+		CLASS_0: ADD R8, #1 ADD R9, #1 				@0x02888001
+		SKIPCLASS_0: CMP R3, #0 				@0x03530000
 			BEQ EXITL1 					@0x08800008
 			SUB R3, #1 					@0x02433001
 			B L1 						@0xE8000084
-		EXITL1: CMP R8, R9 				@0x01580009
-			BMI RETURN_0 				@0x48800008
+		EXITL1: CMP R8, R9 					@0x01580009
+			BMI RETURN_0 					@0x48800008
 			MOVW R0, #0					
 			B RETURN 					@0xE8800004
 		RETURN_0: MOVW R0, #1
